ed57626558a05c78035397b19c16458ead542fa1
jzhupan <jzhupan@gmail.com>
2023-09-20 16:30:00 -0400

    Achieved Scrolling Snap animation.

---
+++ backstage-talks/src/app/Components/StaticText.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+export const StaticText = () => {
+  return (
+    <div className='header-container'>
+        <h1 className='title-text'>BACKSTAGE TALKS</h1> 
+        <h3 className='contact-text'>info@jzhupan.com</h3>   
+        
+  
+        <section className='text-container'>
+        <p>Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right ones, we need to talk.</p>
+        <h6>© 2023 Published by Büro Milk</h6>
+        </section>
+
+
+        <section className='sidebar-nav'>
+          <h2>
+          <a href='/#1'>Issue #1</a>
+          </h2>
+          <h2>
+          <a href='/#2'>Issue #2</a>
+          </h2>
+          <h2>
+          <a href='/#3'>Issue #3</a>
+          </h2>
+          <h2>
+          <a href='/#4'>Issue #4</a>
+          </h2>
+          <h2>
+          <a href='/#5'>Issue #5</a>
+          </h2>
+        </section>
+
+ 
+    <h5 className='footer'>Privacy Policy</h5>
+
+    </div>
+
+  )
+}
